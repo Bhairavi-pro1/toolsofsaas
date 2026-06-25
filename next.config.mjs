f1ad0c106e9d40a5b93a79a162787f93f69b24e6
sarvesh8939 <ssvvb2004@gmail.com>
2026-06-25 15:35:31 +0530

    Revert "test"
    This reverts commit 1c59f3df29e19f9bee5d343f54f48c0ee59812a6.

--- next.config.mjs
+++ next.config.mjs
@@ -9,18 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/errorfixer',
-        destination: 'https://errorfixer002.netlify.app/errorfixer',
-      },
-      {
-        source: '/errorfixer/:path*',
-        destination: 'https://errorfixer002.netlify.app/errorfixer/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
